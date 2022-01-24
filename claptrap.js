7d9eb1f41e342ae000d5dc92dc3d3a64641cb827
Koliyoshii <oliver-kusch@gmx.de>
2022-01-24 09:24:23 +0100

    update

--- claptrap.js
+++ claptrap.js
@@ -80,7 +80,7 @@ function init() {
   const gltfLoader = new GLTFLoader();
   gltfLoader.load(
     // resource URL
-    "./claptrap_neu_neu.gltf",
+    "./claptrap_scene.gltf",
     // called when the resource is loaded
     function (gltf) {
       //console.log(gltf);
