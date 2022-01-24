f8be9370b94baa3df458485d58c5e76ef8ee289b
Oliver <oliver-kusch@gmx.de>
2022-01-24 12:31:33 +0100

    update

--- claptrap.js
+++ claptrap.js
@@ -260,13 +260,13 @@ function onSelect() {
     newClaptrap.scale.set(0.05, 0.05, 0.05);
     scene.add(newClaptrap); */
 
-    /* const material = new THREE.MeshPhongMaterial({
+    const material = new THREE.MeshPhongMaterial({
       color: 0xffffff * Math.random(),
     });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.setFromMatrixPosition(findTarget.matrix);
     mesh.scale.y = Math.random() * 2 + 1;
-    scene.add(mesh); */
+    scene.add(mesh);
   }
   /* if (reticle.visible) {
     const material = new THREE.MeshPhongMaterial({
