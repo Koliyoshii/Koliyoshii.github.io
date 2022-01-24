5ff0c2942c29292b921227485bc5bb8dfe7d17cf
Oliver <oliver-kusch@gmx.de>
2022-01-24 12:23:16 +0100

    update

--- claptrap.js
+++ claptrap.js
@@ -80,7 +80,7 @@ function init() {
   const gltfLoader = new GLTFLoader();
   gltfLoader.load(
     // resource URL
-    "./claptrap1.gltf",
+    "./claptrap.gltf",
     // called when the resource is loaded
     function (gltf) {
       //console.log(gltf);
